ead6c7627b93f318d339a3495187f6e3b7837f56
Brayan Mendonça <brayan.neves@gmail.com>
2024-09-03 21:28:49 -0300

    feat: add oh-my-zsh

--- packages/install.sh
+++ packages/install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 . $PACKAGES/zsh.sh
+. $PACKAGES/oh-my-zsh.sh
 
 install_packages() {
   install_zsh
+  install_ohmyzsh
 }
---
+++ packages/oh-my-zsh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+install_ohmyzsh() {
+  info "Installing Oh My Zsh!"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+  info "Done installing Oh My Zsh!"
+  blankln
+}
