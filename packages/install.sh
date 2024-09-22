fde93c265ee73dffea31a1854d4265b720ccb2be
Brayan Mendonça <brayan.neves@gmail.com>
2024-09-22 13:46:12 -0300

    feat: add nvm installation script
    - Add .zsh_local as an extension of .zshrc.
    - Any customization to .zshrc should go there.
    - Call nvm install --lts at the end.

--- packages/install.sh
+++ packages/install.sh
@@ -3,9 +3,11 @@
 . $PACKAGES/zsh.sh
 . $PACKAGES/oh-my-zsh.sh
 . $PACKAGES/powerlevel10k.sh
+. $PACKAGES/nvm.sh
 
 install_packages() {
   install_zsh
   install_ohmyzsh
   install_powerlevel10k
+  install_nvm
 }
