471d4aed1f189124dfb4f01df3a4af10ade83592
Brayan Mendonça <brayan.neves@gmail.com>
2024-09-30 12:36:11 -0300

    fix: fix node versions not being handled correctly

--- config/.nvm.zsh
+++ config/.nvm.zsh
@@ -3,16 +3,19 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm_use() {
-  [[ -z "$NODE_DEFAULT" ]] && (
-    nvm install --lts
-    export NODE_DEFAULT=$(nvm alias default | awk '{print $NF}' | tr -d ')' | sed 's/\x1b\[[0-9;]*m//g')
-  )
+  node_default() {
+    nvm alias default | awk '{print $NF}' | tr -d ')' | sed 's/\x1b\[[0-9;]*m//g'
+  }
 
   if [[ -e .nvmrc ]]; then
     nvm install
   else
-    local current=$(node -v)
-    if [[ $current != $NODE_DEFAULT ]]; then
+    if [[ ! "$(node_default)" =~ "^v" ]]; then
+      nvm install --lts
+    fi
+
+    local current=$(node -v 2> /dev/null)
+    if [[ "$current" != "$(node_default)" ]]; then
       nvm use default
     fi
   fi
--- packages/nvm.sh
+++ packages/nvm.sh
@@ -2,15 +2,16 @@
 
 install_nvm() {
   info "Installing nvm..."
-  
   export NVM_DIR="$HOME/.nvm" && (
     git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
     cd "$NVM_DIR" || exit 1
     git checkout "$(git describe --abbrev=0 --tags --match 'v[0-9]*' "$(git rev-list --tags --max-count=1)")"
   ) && . "$NVM_DIR/nvm.sh"
-
-  nvm install --lts
-  
   info "Finished installing nvm!"
+  blankln
+
+  info "Installing node..."
+  nvm install --lts
+  info "Finished installing node!"
   blankln
 }
